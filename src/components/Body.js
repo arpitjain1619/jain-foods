1e05cd3145ad939c850be344c8bba1da5b9e2967
Arpit Jain <jainarpit1619@gmail.com>
2026-08-06 23:01:37 +0530

    feat: implemented search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,9 @@ import RestaurantCard from "./RestaurantCard";
 import { TOP_RATING, SWIGGY_GET_API_URL } from "../utils/constants";
 
 const Body = () => {
-  let [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
+  const [searchText, setSearchText] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -13,33 +15,58 @@ const Body = () => {
     const restaurantListFetch = await fetch(SWIGGY_GET_API_URL);
     const restaurantListData = await restaurantListFetch.json();
 
-    const restaurants =
+    let restaurants =
       restaurantListData?.data?.cards?.flatMap((card) => {
         const grid = card?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
         return Array.isArray(grid) ? grid : [];
       }) ?? [];
 
-    const restaurantsList = restaurants.filter(
-      (restaurant) => !restaurant["@type"],
-    );
+    restaurants = restaurants.filter((restaurant) => !restaurant["@type"]);
 
-    setFilteredRestaurantList(restaurantsList);
+    setRestaurantList(restaurants);
+    setFilteredRestaurantList(restaurants);
   };
 
   return (
     <div className="body">
       <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const searchedRestaurantList = restaurantList.filter(
+                (restaurant) => {
+                  return restaurant.info.name
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase());
+                },
+              );
+
+              setFilteredRestaurantList(searchedRestaurantList);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            filteredRestaurantList = filteredRestaurantList
+            setSearchText("");
+
+            const topRatedRestaurantList = restaurantList
               .filter((restaurant) => {
                 return restaurant.info.avgRating > TOP_RATING;
               })
               .sort((a, b) => b.info.avgRating - a.info.avgRating);
 
-            setFilteredRestaurantList(filteredRestaurantList);
+            setFilteredRestaurantList(topRatedRestaurantList);
           }}
         >
           Top Rated Restaurants
